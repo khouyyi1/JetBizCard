3b74fafcb6535c3defb72d5ec2aaad4dbb8edf42
khouyyi1 <anasskhouyyi@gmail.com>
2023-06-08 01:54:53 +0200

    commit_2

--- app/src/main/java/com/anasskhouyyi/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/anasskhouyyi/jetbizcard/MainActivity.kt
@@ -4,12 +4,16 @@ package com.anasskhouyyi.jetbizcard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
@@ -19,6 +23,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.anasskhouyyi.jetbizcard.ui.theme.JetBizCardTheme
@@ -53,6 +59,19 @@ fun CreateBizCard(){
             colors = CardDefaults.cardColors(containerColor = Color.White),
             elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)) {
 
+            Surface(modifier = Modifier
+                .size(150.dp)
+                .padding(5.dp),
+                shape = CircleShape,
+                border = BorderStroke(0.5.dp, Color.LightGray),
+                tonalElevation = 4.dp,
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha=0.1f)
+            ){
+                Image(painter = painterResource(id = R.drawable.icone_image_profile),
+                    contentDescription = "Profile Image",
+                    contentScale = ContentScale.Crop
+                )
+            }
         }
     }
 }
