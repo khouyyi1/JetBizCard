f502d40e84dae7a3a6e009ffbe3a315bc110149e
khouyyi1 <anasskhouyyi@gmail.com>
2023-06-08 18:17:27 +0200

    commit_3

--- app/src/main/java/com/anasskhouyyi/jetbizcard/MainActivity.kt
+++ app/src/main/java/com/anasskhouyyi/jetbizcard/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,11 +18,15 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -52,27 +58,57 @@ fun CreateBizCard(){
         .fillMaxWidth()
         .fillMaxHeight()) {
         Card(modifier = Modifier
-            .width(200.dp)
+            .width(412.dp)
             .height(390.dp)
             .padding(12.dp),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             colors = CardDefaults.cardColors(containerColor = Color.White),
             elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)) {
-
-            Surface(modifier = Modifier
-                .size(150.dp)
-                .padding(5.dp),
-                shape = CircleShape,
-                border = BorderStroke(0.5.dp, Color.LightGray),
-                tonalElevation = 4.dp,
-                color = MaterialTheme.colorScheme.onSurface.copy(alpha=0.1f)
-            ){
-                Image(painter = painterResource(id = R.drawable.icone_image_profile),
-                    contentDescription = "Profile Image",
-                    contentScale = ContentScale.Crop
-                )
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .height(350.dp),
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally) {
+                CreatorImageProfile()
+                Divider(thickness = 2.dp, color = Color.Black)
+                CreateInfo()
+                Button(onClick = {/* your onClick listener */}){
+                    Text(text = "Portfolio", style = MaterialTheme.typography.displayMedium)
+                }
+                }
             }
         }
+    }
+
+@Composable
+private fun CreateInfo() {
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(
+            text = "Anass K.",
+            style = MaterialTheme.typography.displayLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(text = "Engineer", modifier = Modifier.padding(0.dp))
+        Text(text = "06 01 24 00 65", modifier = Modifier.padding(0.dp))
+    }
+}
+
+@Composable
+private fun CreatorImageProfile() {
+    Surface(
+        modifier = Modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        tonalElevation = 4.dp,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.icone_image_profile),
+            contentDescription = "Profile Image",
+            contentScale = ContentScale.Crop
+        )
     }
 }
 
@@ -83,5 +119,3 @@ fun DefaultPreview(){
         CreateBizCard()
     }
 }
-
-//colors = CardDefaults.cardColors(contentColor = Color(0xFF78138A))
